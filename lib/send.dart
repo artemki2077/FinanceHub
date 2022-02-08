bac1e4ed86bcf5e7fe4e4c7be27ea97c78c75470
artemki2077 <artem.kiselev.2005mail.ru@gmail.com>
2022-02-09 00:40:16 +0300

    add send.dart

---
+++ lib/send.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'config.dart' as conf;
+
+class Send extends StatefulWidget {
+  const Send({Key? key, required this.b}) : super(key: key);
+
+  final bool b;
+  @override
+  _SendState createState() => _SendState();
+}
+
+class _SendState extends State<Send> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
